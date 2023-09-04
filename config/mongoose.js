017a61c233d0f417f697f312a43caf265f2bc9b4
Mukesh <munnamukeshcr7@gmail.com>
2023-09-04 14:14:32 +0530

    updated

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/HOSPITAL_API');
+  await mongoose.connect('mongodb+srv://mukeshaero43:mukeshaero43@cluster0.ogaqhy5.mongodb.net/HOSPITAL_API?retryWrites=true&w=majority');
   console.log('MongoDB connected successfully');
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
